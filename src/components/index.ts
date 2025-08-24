8af2e92b82c643c588c8efed83dafebd341c8ae0
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-08-24 21:26:44 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,20 @@
 // src/components/index.ts - Central export file for all design system components
 
 // Common Components
-export { default as Button } from './common/Button/Button';
-export { default as Card } from './common/Card/Card';
-export { default as Badge } from './common/Badge/Badge';
-export { default as Table } from './common/Table/Table';
-export { default as Modal } from './common/Modal/Modal';
+export { default as Button } from './Button/Button';
+export { default as Card } from './Card/Card';
+export { default as Badge } from './Badge/Badge';
+export { default as Table } from './Table/Table';
+export { default as Modal } from './Modal/Modal';
+export { default as Header } from './Header/Header'; 
+export { default as Footer } from './Footer/Footer'; 
+
+// Export types
+export type { ButtonProps } from './Button/Button.types';
+export type { CardProps } from './Card/Card.types';
+export type { BadgeProps } from './Badge/Badge.types';
+export type { TableProps } from './Table/Table.types';
+export type { ModalProps } from './Modal/Modal.types';
 
 // Type exports
 export type { ButtonProps, ButtonVariant, ButtonSize } from './common/Button/Button.types';
