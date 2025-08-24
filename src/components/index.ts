919f77e906c8de9c1a3828d44b77055632d51bae
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-08-24 21:35:52 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,27 +1,21 @@
-// src/components/index.ts - Central export file for all design system components
+// src/components/index.ts
+// Fixed paths to match your actual directory structure
 
-// Common Components
-export { default as Button } from './Button/Button';
-export { default as Card } from './Card/Card';
-export { default as Badge } from './Badge/Badge';
-export { default as Table } from './Table/Table';
-export { default as Modal } from './Modal/Modal';
-export { default as Header } from './Header/Header'; 
-export { default as Footer } from './Footer/Footer'; 
+// Common Components - Import from ./common/ subdirectory
+export { default as Button } from './common/Button/Button';
+export { default as Card } from './common/Card/Card';
+export { default as Badge } from './common/Badge/Badge';
+export { default as Table } from './common/Table/Table';
+export { default as Modal } from './common/Modal/Modal';
+export { default as Header } from './common/Header/Header';
+export { default as Footer } from './common/Footer/Footer';
 
-// Export types
-export type { ButtonProps } from './Button/Button.types';
-export type { CardProps } from './Card/Card.types';
-export type { BadgeProps } from './Badge/Badge.types';
-export type { TableProps } from './Table/Table.types';
-export type { ModalProps } from './Modal/Modal.types';
+// Export all component types
+export type { ButtonProps } from './common/Button/Button.types';
+export type { CardProps } from './common/Card/Card.types';
+export type { BadgeProps } from './common/Badge/Badge.types';
+export type { TableProps } from './common/Table/Table.types';
+export type { ModalProps } from './common/Modal/Modal.types';
 
-// Type exports
-export type { ButtonProps, ButtonVariant, ButtonSize } from './common/Button/Button.types';
-export type { CardProps, CardHeaderProps, CardBodyProps, CardFooterProps } from './common/Card/Card.types';
-export type { BadgeProps, BadgeVariant } from './common/Badge/Badge.types';
-export type { TableProps, Column, SortOrder } from './common/Table/Table.types';
-export type { ModalProps, ModalHeaderProps, ModalBodyProps, ModalFooterProps } from './common/Modal/Modal.types';
-
-// Design tokens
+// Re-export design tokens for convenience
 export { designTokens } from '../styles/designTokens';
