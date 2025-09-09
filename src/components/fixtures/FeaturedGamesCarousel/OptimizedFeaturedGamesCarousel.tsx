5ca6832e1e92b6c08bef8105c92b0b1c7459a797
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-09 13:35:09 +0100

    Update OptimizedFeaturedGamesCarousel.tsx

--- src/components/fixtures/FeaturedGamesCarousel/OptimizedFeaturedGamesCarousel.tsx
+++ src/components/fixtures/FeaturedGamesCarousel/OptimizedFeaturedGamesCarousel.tsx
@@ -183,7 +183,8 @@ const OptimizedFeaturedGamesCarousel: React.FC<Props> = ({
             </SwiperSlide>
           );
         })}
-      </Swiper>
+              </Swiper>
+      </div> {/* Close padding wrapper */}
 
       {/* Centered Pagination below */}
       <div className="swiper-pagination-container mt-[var(--space-md)] flex justify-center" />
