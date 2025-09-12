a7668115b45f5aa1f1cf42e760179d0b619fea5f
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-12 21:07:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/globals.css'; // Add this line
 import HomePage from './pages/HomePage';
-import StatsPage from './pages/StatsPage
+import StatsPage from './pages/StatsPage;
 
 function App() {
   return (
