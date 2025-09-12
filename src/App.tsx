cf2e97b7e7df02be89f2cff0081e7475c3df5dab
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-12 21:00:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/globals.css'; // Add this line
 import HomePage from './pages/HomePage';
+import StatsPage from './pages/StatsPage
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Your existing home page route */}
+          <Route path="/" element={<HomePage />} />
+          
+          {/* New dynamic stats page route */}
+          <Route path="/stats/:matchId" element={<StatsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
+
+ 
