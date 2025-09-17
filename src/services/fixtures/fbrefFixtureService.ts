0a20e7fe042cc0c6f7ac9a475b0617192cf79825
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-17 08:13:28 +0100

    Update fbrefFixtureService.ts

--- src/services/fixtures/fbrefFixtureService.ts
+++ src/services/fixtures/fbrefFixtureService.ts
@@ -487,7 +487,7 @@ export class FBrefFixtureService {
   }> {
     if (!this.isCacheValid()) await this.refreshCache();
     
-    const allFixtures = this.fixturesCache;
+    // const allFixtures = this.fixturesCache;
     const currentWeekFixtures = await this.getCurrentGameWeekFixtures();
     
     if (currentWeekFixtures.length === 0) {
