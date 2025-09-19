eee4f1b304c4eade93b6128595622d1b51be7634
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-19 10:24:21 +0100

    Update fbrefStatsService.ts

--- src/services/stats/fbrefStatsService.ts
+++ src/services/stats/fbrefStatsService.ts
@@ -1,7 +1,7 @@
 // src/services/stats/fbrefStatsService.ts
 import { fbrefScraper, type ScrapedData, type TableData } from '../scrape/Fbref';
 import { normalizeTeamName } from '../../utils/teamUtils';
-import { fbrefTeamMatchLogsService, PREMIER_LEAGUE_TEAMS, type TeamSeasonCorners } from './fbrefMatchLogsService';
+import { fbrefMatchLogsService, PREMIER_LEAGUE_TEAMS, type TeamSeasonCorners } from './fbrefMatchLogsService';
 
 interface TeamFormData {
   homeResults: ('W' | 'D' | 'L')[];
