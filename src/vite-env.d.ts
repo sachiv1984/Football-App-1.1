f4a134d3ea1cc9c64b2406d868d9fa8d749ce4e0
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-20 14:59:12 +0100

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,16 @@
+// src/vite-env.d.ts (create this file if it doesn't exist)
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly MODE: string
+  readonly BASE_URL: string
+  readonly PROD: boolean
+  readonly DEV: boolean
+  readonly SSR: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
