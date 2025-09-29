a1cfa6ec9c8ce5c9e5e6f07f516f33b78f400889
sachiv1984 <sachiv.patel@hotmail.co.uk>
2025-09-29 21:26:50 +0100

    Update odds.ts

--- api/odds.ts
+++ api/odds.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { oddsAPIService } from '../src/services/api/oddsAPIService';
+import { oddsAPIService } from '../../services/api/oddsAPIService';
+
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
